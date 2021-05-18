17f4f796557471c855a0db2bb45f0d08c990c82e
gabrielius.kazakevicius <gabrielius.kazakevicius@telesoftas.com>
2021-05-18 13:32:03 +0300

    RacoonSecure.Identity package release

---
+++ RacoonSecure/RacoonSecure.Core.Identity/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RacoonSecure.Core.Identity
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Register RacoonSecurePasswordValidator as Identity IPasswordValidator.
+        /// Warning! Call this method before calling services.AddIdentity $lt;TUser, TRole&gt;() if you want
+        /// to override default password validation.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="validator">RacoonSecure password validator</param>
+        /// <typeparam name="TUser">Type of IdentityUser used in IdentityContext</typeparam>
+        public static void AddRacoonSecurePasswordValidator<TUser>(this IServiceCollection services, PasswordValidator validator)
+            where TUser : IdentityUser
+        {
+            services.AddSingleton<IPasswordValidator<TUser>, RacoonSecurePasswordValidator<TUser>>(
+                _ => new RacoonSecurePasswordValidator<TUser>(validator)
+            );
+        }
+    }
+}
